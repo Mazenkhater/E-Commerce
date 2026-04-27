b5fcf5c4fb3ca67a33f672b3080023e471e44380
Mazen Osama <engmazenkhater@gmail.com>
2026-04-27 15:18:00 +0300

    some modifications

--- E _Commerce/E_Commerce.Service/obj/Debug/net8.0/E_Commerce.Service.AssemblyInfo.cs
+++ E _Commerce/E_Commerce.Service/obj/Debug/net8.0/E_Commerce.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E_Commerce.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2152e8a93dbdaefe62a1acc6ed433b3a1eb3f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("E_Commerce.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E_Commerce.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
